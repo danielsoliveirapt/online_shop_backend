068a0b9693c1b8013f6960eaae527e11bd7c5dd3
Daniel Oliveira <danielsoliveirapt@gmail.com>
2023-02-23 01:26:20 +0000

    first commit

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  productId: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
